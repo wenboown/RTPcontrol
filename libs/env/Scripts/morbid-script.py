50cd72659426ea636683e0003a57e99c72858a29
Bo Wen <wenboown@gmail.com>
2013-09-23 02:45:17 -0400

    Add libs

---
+++ libs/env/Scripts/morbid-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\Bohr\PycharmProjects\RTP\libs\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'morbid==0.8.7.3','console_scripts','morbid'
+__requires__ = 'morbid==0.8.7.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('morbid==0.8.7.3', 'console_scripts', 'morbid')()
+    )
